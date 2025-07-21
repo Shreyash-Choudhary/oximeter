e16399db205a5df7239fd27f03c04811b792fe22
Shreyash-Choudhary <shreyashchoudhary39@gmail.com>
2025-07-22 00:05:45 +0530

    update

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def analyze_video():
             os.remove(video_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
